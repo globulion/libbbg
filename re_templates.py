27d95526178450fc3f70875aa7312a660fea4c88
globulion <blasiak.bartosz@gmail.com>
2013-09-26 15:38:54 +0900

    FORTRAN double precision format added in re_templates module

--- re_templates.py
+++ re_templates.py
@@ -4,3 +4,6 @@ __all__ = ['re_real_e']
 
 # scientific notation with E/e
 re_real_e=r'-?\d\.\d+[Ee][+\-]\d\d?'
+
+# FORTRAN double precision number format
+re_dbl_fort = re.compile(r'(\d*\.\d+)[dD]([-+]?\d+)')
