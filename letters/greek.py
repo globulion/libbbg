a66d9aff9e9e485c4927750677cd3e22cf6c3404
globulion <blasiak.bartosz@gmail.com>
2014-03-12 20:50:05 +0900

    letters submodule added to libbbg

---
+++ letters/greek.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""
+Greek letters
+
+Version 1.0
+"""
+# lower-cases
+alpha   = unichr(0x3B1).encode('utf-8')
+beta    = unichr(0x3B2).encode('utf-8')
+gamma   = unichr(0x3B3).encode('utf-8')
+delta   = unichr(0x3B4).encode('utf-8')
+epsilon = unichr(0x3B5).encode('utf-8')
+
+# upper-cases
+Alpha   = unichr(0x391).encode('utf-8')
+Beta    = unichr(0x392).encode('utf-8')
+Gamma   = unichr(0x393).encode('utf-8')
+Delta   = unichr(0x394).encode('utf-8')
+Epsilon = unichr(0x395).encode('utf-8')
