5c9c06bd33c5d26930e44d196cf4f383c1d54eac
globulion <blasiak.bartosz@gmail.com>
2014-11-12 15:54:11 +0900

    Added __init__.py and rebasing

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+__version__='1.0'
+__author__='Bartosz Błasiak'
+__all__=['units','utilities','utilities2','dma','gaussfreq',
+         'dipderiv','clemtp','pmloca','letters','fourier']
