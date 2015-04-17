bbc3b7bcad60c19fc46fe84b33a7fbc22298f9b7
globulion <blasiak.bartosz@gmail.com>
2015-04-17 22:52:48 +0900

    Rotation bug fixed; superimposition fixed

--- libbbg/dma.py
+++ libbbg/dma.py
@@ -17,6 +17,129 @@ __all__ = ['DMA']
 #from units import *
 import numpy, numpy.linalg, copy, units, utilities2
 
+### SVDSuperimposer from BIOPYTHON PACKAGE
+# Copyright (C) 2002, Thomas Hamelryck (thamelry@vub.ac.be)
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+# [!] zmiana w oryginalnym kodzie:
+#        w run() zmieniono na:     
+#           av1=sum(coords,axis=0)/self.n  
+#           av2=sum(reference_coords,axis=0)/self.n 
+#        z powodu złej wersji metody sum()
+class SVDSuperimposer(object):
+    """
+    >>>>> Copied from LIBBBG.utilities <<<<<
+    SVDSuperimposer finds the best rotation and translation to put
+    two point sets on top of each other (minimizing the RMSD). This is 
+    eg. useful to superimpose crystal structures.  
+
+    SVD stands for Singular Value Decomposition, which is used to calculate
+    the superposition.
+
+    Reference:
+
+    Matrix computations, 2nd ed. Golub, G. & Van Loan, CF., The Johns 
+    Hopkins University Press, Baltimore, 1989
+    """
+    def __init__(self):
+        self._clear()
+
+    # Private methods
+
+    def _clear(self):
+        self.reference_coords=None
+        self.coords=None
+        self.transformed_coords=None
+        self.rot=None
+        self.tran=None
+        self.rms=None
+        self.init_rms=None
+
+    def _rms(self, coords1, coords2):
+        "Return rms deviations between coords1 and coords2."
+        diff=coords1-coords2
+        l=coords1.shape[0]
+        return numpy.sqrt(numpy.sum(numpy.sum(diff*diff))/l)
+
+    # Public methods
+    
+    def set(self, reference_coords, coords):
+        """
+        Set the coordinates to be superimposed.
+        coords will be put on top of reference_coords.
+
+        o reference_coords: an NxDIM array
+        o coords: an NxDIM array
+
+        DIM is the dimension of the points, N is the number
+        of points to be superimposed.
+        """
+        # clear everything from previous runs
+        self._clear()
+        # store cordinates
+        self.reference_coords=reference_coords
+        self.coords=coords
+        n=reference_coords.shape
+        m=coords.shape
+        if n!=m or not(n[1]==m[1]==3):
+            raise Exception("Coordinate number/dimension mismatch.")
+        self.n=n[0]
+
+    def run(self):
+        "Superimpose the coordinate sets."
+        if self.coords is None or self.reference_coords is None:
+            raise Exception("No coordinates set.")
+        coords=self.coords
+        reference_coords=self.reference_coords
+        # center on centroid
+        av1=numpy.sum(coords,axis=0)/self.n  
+        av2=numpy.sum(reference_coords,axis=0)/self.n    
+        coords=coords-av1
+        reference_coords=reference_coords-av2
+        # correlation matrix
+        a=numpy.dot(numpy.transpose(coords), reference_coords)
+        u, d, vt=numpy.linalg.svd(a)
+        self.rot=numpy.transpose(numpy.dot(numpy.transpose(vt), numpy.transpose(u)))
+        # check if we have found a reflection
+        if numpy.linalg.det(self.rot)<0:
+            vt[2]=-vt[2]
+            self.rot=numpy.transpose(numpy.dot(numpy.transpose(vt), numpy.transpose(u)))
+        self.tran=av2-numpy.dot(av1, self.rot)
+
+    def get_transformed(self):
+        "Get the transformed coordinate set."
+        if self.coords is None or self.reference_coords is None:
+            raise Exception("No coordinates set.")
+        if self.rot is None:
+            raise Exception("Nothing superimposed yet.")
+        if self.transformed_coords is None:
+            self.transformed_coords=numpy.dot(self.coords, self.rot)+self.tran
+        return self.transformed_coords
+
+    def get_rotran(self):
+        "Right multiplying rotation matrix and translation."
+        if self.rot is None:
+            raise Exception("Nothing superimposed yet.")
+        return self.rot, self.tran
+
+    def get_init_rms(self):
+        "Root mean square deviation of untransformed coordinates."
+        if self.coords is None:
+            raise Exception("No coordinates set yet.")
+        if self.init_rms is None:
+            self.init_rms=self._rms(self.coords, self.reference_coords)
+        return self.init_rms
+
+    def get_rms(self):
+        "Root mean square deviation of superimposed coordinates."
+        if self.rms is None:
+            transformed_coords=self.get_transformed()
+            self.rms=self._rms(transformed_coords, self.reference_coords)
+        return self.rms
+
+
 def ParseDMA(file,type='coulomb',hexadecapoles=True):
     """\
 >>>>> Copied from LIBBBG.utilities <<<<<
@@ -907,8 +1030,10 @@ be chosen.
 
 Returned value is an RMS of a superimposition and is given in Bohrs.
 """
-        suplist = numpy.array(suplist)-1
-        s = utilities.SVDSuperimposer()
+        # convert suplist to Python-convention indices
+        if suplist is not None: suplist = numpy.array(suplist)-1
+        # begin superimposition
+        s = SVDSuperimposer()
         xyz_prev = self.pos
         if sup_origin: xyz_prev = self.origin
         if len(xyz)!=0:
@@ -1751,10 +1876,10 @@ The numbers are normal numbers (not in Python convention)."""
                   hexadecapole[i] = numpy.tensordot(rotmat.T,numpy.tensordot(
                                               rotmat.T,numpy.tensordot(
                                               rotmat.T,numpy.tensordot(
-                                              rotmat.T,self.DMA_FULL[5][i] , (1,2)
-                                            ),(1,2)
-                                            ),(1,2)
-                                            ),(1,2)
+                                              rotmat.T,self.DMA_FULL[5][i] , (1,3)
+                                            ),(1,3)
+                                            ),(1,3)
+                                            ),(1,3)
                                             )
 
               self.DMA_FULL[5] = hexadecapole
