f6d8e3aa4f615b856fd07539ca5de26fdd00fd65
globulion <blasiak.bartosz@gmail.com>
2019-07-15 18:50:07 +0200

    Licence for SVDSuperimposer updated

--- libbbg/utilities.py
+++ libbbg/utilities.py
@@ -12,7 +12,7 @@ __all__=['SVDSuperimposer','ParseDMA','RotationMatrix',
          'CalcStep','ModifyStruct','ParseUnitedAtoms',
          'MakeSoluteAndSolventFiles','GROUPS','DistanceRelationMatrix',
          'status','ROTATE','get_tcf','choose','get_pmloca','get_elmtp',
-         'ParseVecFromFchk','interchange','Peak','PUPA','VIB',
+         'ParseVecFromFchk','interchange','Peak','PRINTHELPER','VIB',
          'ParseFCFromFchk','ParseDipoleDerivFromFchk',
          'ParseFockFromGamessLog','lind','order','check_sim','MakeMol',
          'ParseDistributedPolarizabilitiesFromGamessEfpFile','reorder',
@@ -4712,19 +4712,95 @@ def DistanceRelationMatrix(xyz,threshold=1):
                
     return A_ij
 
-### SVDSuperimposer from BIOPYTHON PACKAGE
-# Copyright (C) 2002, Thomas Hamelryck (thamelry@vub.ac.be)
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
-
-# [!] zmiana w oryginalnym kodzie:
-#        w run() zmieniono na:     
-#           av1=sum(coords,axis=0)/self.n  
-#           av2=sum(reference_coords,axis=0)/self.n 
-#        z powodu złej wersji metody sum()
 class SVDSuperimposer(object):
     """
+     SVDSuperimposer adapted from BIOPYTHON PACKAGE
+
+     Copyright (C) 2002, Thomas Hamelryck (thamelry@vub.ac.be)
+     This code is part of the Biopython distribution and governed by its
+     license.  Please see the LICENSE file that should have been included
+     as part of this package.
+
+     ------ LICENCE ------ 
+
+     Biopython is currently released under the "Biopython License Agreement"
+     (given in full below). Unless stated otherwise in individual file headers,
+     all Biopython's files are under the "Biopython License Agreement".
+     
+     Some files are explicitly dual licensed under your choice of the
+     "Biopython License Agreement" or the "BSD 3-Clause License" (both given
+     in full below). This is with the intention of later offering all of
+     Biopython under this dual licensing approach.
+     
+     
+     ------------------------------------------------------------------------------
+     
+     
+     Biopython License Agreement
+     ---------------------------
+     
+     Permission to use, copy, modify, and distribute this software and its
+     documentation with or without modifications and for any purpose and
+     without fee is hereby granted, provided that any copyright notices
+     appear in all copies and that both those copyright notices and this
+     permission notice appear in supporting documentation, and that the
+     names of the contributors or copyright holders not be used in
+     advertising or publicity pertaining to distribution of the software
+     without specific prior permission.
+     
+     THE CONTRIBUTORS AND COPYRIGHT HOLDERS OF THIS SOFTWARE DISCLAIM ALL
+     WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
+     WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL THE
+     CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT
+     OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+     OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+     OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+     OR PERFORMANCE OF THIS SOFTWARE.
+     
+     
+     ------------------------------------------------------------------------------
+     
+     
+     BSD 3-Clause License
+     --------------------
+     
+     Copyright (c) 1999-2019, The Biopython Contributors
+     All rights reserved.
+     
+     Redistribution and use in source and binary forms, with or without
+     modification, are permitted provided that the following conditions are met:
+     
+     1. Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimer.
+     
+     2. Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+     
+     3. Neither the name of the copyright holder nor the names of its contributors
+        may be used to endorse or promote products derived from this software
+        without specific prior written permission.
+     
+     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+     AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+     IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+     FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+     DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+     SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+     CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+     OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+     OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+     ------
+    
+     [!] zmiana w oryginalnym kodzie:
+            w run() zmieniono na:     
+               av1=sum(coords,axis=0)/self.n  
+               av2=sum(reference_coords,axis=0)/self.n 
+            z powodu złej wersji metody sum()
+
+    ----
+
     SVDSuperimposer finds the best rotation and translation to put
     two point sets on top of each other (minimizing the RMSD). This is 
     eg. useful to superimpose crystal structures.  
@@ -6953,7 +7029,7 @@ def PRINTV(M,list1,list2,list3):
              print(': %4s' % t3.rjust(4))
            print()
            
-def PUPA(a):
+def PRINTHELPER(a):
     """ print helper 1 """
     log=""
     for i in range(len(a)):
